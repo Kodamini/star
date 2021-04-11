01a495ab846906f10c7088b75d69d07f082114f8
Kodamini <72760104+Kodamini@users.noreply.github.com>
2021-04-11 20:38:59 +0300

    Initial commit

---
+++ src/sample/Controller/HolaController.java
@@ -0,0 +1,23 @@
+package sample.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class HolaController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Text HolaAmigo;
+
+    @FXML
+    void initialize() {
+
+    }
+}
